eda9135e183e5ff4b67b09613b03f64ca2b02b67
Simon Lizotte <simon.lizotte.1@ulaval.ca>
2021-11-16 22:36:48 -0500

    add tests for constructor exceptions

--- include/FastMIDyNet/proposer/multiplemoves.hpp
+++ include/FastMIDyNet/proposer/multiplemoves.hpp
@@ -13,12 +13,12 @@ template<typename MoveType>
 class MultipleMovesProposer: public Proposer<MoveType> {
     RNG& m_rng;
     std::vector<Proposer<MoveType>*>& m_proposers;
-    std::vector<double> m_moveProbabilities;
+    std::vector<double> m_moveWeights;
     unsigned int m_proposedMoveType=0;
     std::discrete_distribution<unsigned int> m_moveTypeDistribution;
 
     public:
-        MultipleMovesProposer(std::vector<Proposer<MoveType>*>& proposers, std::vector<double> moveProbabilities, RNG& rng);
+        MultipleMovesProposer(std::vector<Proposer<MoveType>*>& proposers, std::vector<double> moveWeights, RNG& rng);
         MoveType operator()();
         double getProposalProb(const MoveType&) const;
         void updateProbabilities(const MoveType&);
@@ -26,16 +26,16 @@ class MultipleMovesProposer: public Proposer<MoveType> {
 
 template<typename MoveType>
 MultipleMovesProposer<MoveType>::MultipleMovesProposer(std::vector<Proposer<MoveType>*>& proposers,
-    std::vector<double> moveProbabilities, RNG& rng):
-            m_proposers(proposers), m_rng(rng), m_moveProbabilities(moveProbabilities) {
+    std::vector<double> moveWeights, RNG& rng):
+            m_proposers(proposers), m_rng(rng), m_moveWeights(moveWeights) {
 
-    if (m_moveProbabilities.size() != m_proposers.size())
+    if (m_moveWeights.size() != m_proposers.size())
         throw std::invalid_argument("MultipleMovesProposer: Number of "
-                "proposers isn't equal to the number of moveProbabilities.");
+                "proposers isn't equal to the number of moveWeights.");
     if (m_proposers.size() == 0)
         throw std::invalid_argument("MultipleMovesProposer: No proposers given.");
 
-    m_moveTypeDistribution = std::discrete_distribution<unsigned int>(moveProbabilities.begin(), moveProbabilities.end());
+    m_moveTypeDistribution = std::discrete_distribution<unsigned int>(moveWeights.begin(), moveWeights.end());
 }
 
 template<typename MoveType>
