4a5ada7078d3c93aacc765d85ccfd2b1427d0369
Simon Lizotte <simon.lizotte.1@ulaval.ca>
2021-11-17 11:37:19 -0500

    comment methods to implement in child objects

--- include/FastMIDyNet/prior/prior.hpp
+++ include/FastMIDyNet/prior/prior.hpp
@@ -15,20 +15,18 @@ class Prior{
         void setState(const T& state) { m_state = state; }
 
         virtual T sample() = 0;
-        virtual double getLogLikelihood(const T& prior) const = 0;
-        virtual double getLogLikelihood() const { return getLogLikelihood(m_state); }
-        double getLogPrior() const { return 0; }
+        virtual double getLogLikelihood() const = 0;
+        virtual double getLogPrior() const = 0;
         double getLogJoint() const { return getLogPrior() + getLogLikelihood(); }
-        double getLogLikelihoodRatio(const GraphMove& move) const {
-            T newState = getStateAfterMove(move);
-            return getLogLikelihood(newState) - getLogLikelihood();
-        }
+        //double getLogLikelihoodRatio(const GraphMove& move) const {
+            //T newState = getStateAfterMove(move);
+            //return getLogLikelihood(newState) - getLogLikelihood();
+        //}
 
-        void applyMove(const GraphMove&) { setState(getStateAfterMove(move)); }
-        virtual T getStateAfterMove(const GraphMove&) = 0;
+        //void applyMove(const GraphMove& move) { setState(getStateAfterMove(move)); }
+        //virtual T getStateAfterMove(const GraphMove&) = 0;
 
         virtual void checkConsistency() = 0;
-
 
     protected:
         T m_state;
