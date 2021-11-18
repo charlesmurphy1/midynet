4cdcf8dec3d5723c30b9b4a95aaa67ee8a95c9d9
Simon Lizotte <simon.lizotte.1@ulaval.ca>
2021-11-18 09:58:11 -0500

    match class with new prior structure

--- include/FastMIDyNet/prior/dcsbm/degree_count.h
+++ include/FastMIDyNet/prior/dcsbm/degree_count.h
@@ -8,16 +8,28 @@
 namespace FastMIDyNet{
 
 class DegreeCountPrior: public Prior<DegreeSequence>{
-    const EdgeCountPrior& m_edgeCountPrior;
+    EdgeCountPrior& m_edgeCountPrior;
     size_t m_graphSize;
 
     public:
-        DegreeCountPrior(const EdgeCountPrior& edgeCountPrior, size_t graphSize):
+        DegreeCountPrior(EdgeCountPrior& edgeCountPrior, size_t graphSize):
             m_edgeCountPrior(edgeCountPrior), m_graphSize(graphSize) { }
+
+
+        double getLogPrior() { return m_edgeCountPrior.getLogJoint(); }
+        virtual double getLogLikelihoodRatio(const GraphMove& move) const = 0;
+        double getLogJointRatio(const GraphMove& move) {
+            double logJointRatio = 0;
+            if (!isProcessed)
+                logJointRatio = getLogLikelihoodRatio(move) + m_edgeCountPrior.getLogJointRatio(move);
+            isProcessed = true;
+            return logJointRatio;
+        }
+        double getLogJointRatio(const BlockMove& move) { return 0; }
+
 
         double getLogLikelihoodRatio(const BlockMove&) const { return 0; }
         void applyMove(const BlockMove&) { }
-        double getLogPrior() const { return m_edgeCountPrior.getLogLikelihood(); }
 };
 
 
