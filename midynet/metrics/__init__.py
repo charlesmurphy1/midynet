ef625952da1ab7c629d623060005479b357c2f5c
Charles Murphy <charles.murphy.1@ulaval.ca>
2022-01-11 08:46:06 -0500

    init metrics submodule

---
+++ midynet/metrics/__init__.py
@@ -0,0 +1,5 @@
+from .metrics import *
+from .dynamics_prediction_entropy import *
+from .graph_entropy import *
+from .graph_reconstruction_entropy import *
+from .dynamics_entropy import *
---
+++ midynet/metrics/graph_entropy.py
@@ -0,0 +1,4 @@
+from .metrics import Metrics
+
+if __name__ == "__main__":
+    pass
