a93a66a1d2153fa7d281333cfa22462f7bf0cace
charlesmurphy1 <charles.murphy.1@ulaval.ca>
2022-01-14 17:58:14 -0500

    clean up

--- tests/scripts/test_script.py
+++ tests/scripts/test_script.py
@@ -14,18 +14,6 @@ class TestScriptManager(unittest.TestCase):
             env_to_load="./env/midynet-env/bin/activate",
         )
 
-        # name: str
-        # executable: str
-        # config: Config = field(repr=False, default_factory=Config)
-        # config_array: list[Config] = field(repr=False, default_factory=list)
-        # execution_command: str = field(repr=False, default="bash")
-        # resources: dict[str, str] = field(repr=False, default_factory=dict)
-        # resource_prefix: str = field(repr=False, default="#SBATCH")
-        # path_to_script: pathlib.Path = field(repr=False, default_factory=pathlib.Path)
-        # path_to_scripts: list[pathlib.Path] = field(repr=False, default_factory=list)
-        # modules_to_load: list[str] = field(repr=False, default_factory=list)
-        # env_to_load: str = None
-
     def test_write_script(self):
         print(self.script.write_script(self.config))
 
