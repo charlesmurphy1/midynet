4f117514b1245bc9306d068cb757530d0fd8eeab
Charles Murphy <charles.murphy.1@ulaval.ca>
2022-01-22 13:24:57 -0500

    adding other dynamics to fig2 script

--- scripts/midynet/figure2-exact.py
+++ scripts/midynet/figure2-exact.py
@@ -3,20 +3,23 @@ from config import *
 
 
 def main():
-    config = get_config_figure2Exact("sis", num_procs=32, time="24:00:00", mem=12)
-    script = midynet.scripts.ScriptManager(
-        executable=PATH_TO_RUN_EXEC["run"],
-        execution_command=EXECUTION_COMMAND,
-        path_to_scripts="./scripts",
-    )
-    script.run(
-        config,
-        resources=config.resources,
-        modules_to_load=SPECS["modules_to_load"],
-        virtualenv=SPECS["virtualenv"],
-        extra_args=dict(verbose=2),
-        teardown=False,
-    )
+    for dynamics in ["sis", "cowan", "ising"]:
+        config = get_config_figure2Exact(
+            dynamics, num_procs=32, time="24:00:00", mem=12
+        )
+        script = midynet.scripts.ScriptManager(
+            executable=PATH_TO_RUN_EXEC["run"],
+            execution_command=EXECUTION_COMMAND,
+            path_to_scripts="./scripts",
+        )
+        script.run(
+            config,
+            resources=config.resources,
+            modules_to_load=SPECS["modules_to_load"],
+            virtualenv=SPECS["virtualenv"],
+            extra_args=dict(verbose=2),
+            teardown=False,
+        )
 
 
 if __name__ == "__main__":
--- scripts/midynet/figure2-mcmc.py
+++ scripts/midynet/figure2-mcmc.py
@@ -3,31 +3,32 @@ from config import *
 
 
 def main():
-    config = get_config_figure2MCMC("sis", num_procs=4, mem=12)
-    script = midynet.scripts.ScriptManager(
-        executable=PATH_TO_RUN_EXEC["run"],
-        execution_command=EXECUTION_COMMAND,
-        path_to_scripts="./scripts",
-    )
-    ais_config, mf_config = script.split_param(config, "metrics.mutualinfo.method")
+    for dynamics in ["sis", "cowan", "ising"]:
+        config = get_config_figure2MCMC(dynamics, num_procs=32, mem=12)
+        script = midynet.scripts.ScriptManager(
+            executable=PATH_TO_RUN_EXEC["run"],
+            execution_command=EXECUTION_COMMAND,
+            path_to_scripts="./scripts",
+        )
+        ais_config, mf_config = script.split_param(config, "metrics.mutualinfo.method")
 
-    mf_config.resources["time"] = "24:00:00"
-    script.run(
-        mf_config,
-        resources=mf_config.resources,
-        modules_to_load=SPECS["modules_to_load"],
-        virtualenv=SPECS["virtualenv"],
-        extra_args=dict(verbose=2),
-    )
+        mf_config.resources["time"] = "24:00:00"
+        script.run(
+            mf_config,
+            resources=mf_config.resources,
+            modules_to_load=SPECS["modules_to_load"],
+            virtualenv=SPECS["virtualenv"],
+            extra_args=dict(verbose=2),
+        )
 
-    ais_config.resources["time"] = "48:00:00"
-    script.run(
-        ais_config,
-        resources=ais_config.resources,
-        modules_to_load=SPECS["modules_to_load"],
-        virtualenv=SPECS["virtualenv"],
-        extra_args=dict(verbose=2),
-    )
+        ais_config.resources["time"] = "48:00:00"
+        script.run(
+            ais_config,
+            resources=ais_config.resources,
+            modules_to_load=SPECS["modules_to_load"],
+            virtualenv=SPECS["virtualenv"],
+            extra_args=dict(verbose=2),
+        )
 
 
 if __name__ == "__main__":
