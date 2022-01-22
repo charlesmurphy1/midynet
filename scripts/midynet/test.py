0ccddc96315dd12eb1a87f9cb5c474fd36666c51
Charles Murphy <charles.murphy.1@ulaval.ca>
2022-01-22 13:13:09 -0500

    adding tests for script

--- scripts/midynet/test.py
+++ scripts/midynet/test.py
@@ -3,7 +3,7 @@ from config import *
 
 
 def main():
-    config = get_config_figure2Exact("sis", num_procs=4, time="24:00:00", mem=12)
+    config = get_config_test(num_procs=4, time="24:00:00", mem=12)
     script = midynet.scripts.ScriptManager(
         executable=PATH_TO_RUN_EXEC["run"],
         execution_command=EXECUTION_COMMAND,
@@ -14,7 +14,7 @@ def main():
         resources=config.resources,
         modules_to_load=SPECS["modules_to_load"],
         virtualenv=SPECS["virtualenv"],
-        extra_args=dict(verbose=2),
+        extra_args=dict(verbose=1),
         teardown=False,
     )
 
