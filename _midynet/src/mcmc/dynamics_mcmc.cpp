6e87738d6b60f3dfc53566e18554847eb30e2e29
charlesmurphy1 <charles.murphy.1@ulaval.ca>
2022-01-29 13:52:38 -0500

    clean up

--- _midynet/src/mcmc/dynamics_mcmc.cpp
+++ _midynet/src/mcmc/dynamics_mcmc.cpp
@@ -16,11 +16,7 @@ bool DynamicsMCMC::doMetropolisHastingsStep() {
     else {
         m_lastMoveWasGraphMove = true;
 
-        std::cout << "proposing move" << std::endl;
-
         GraphMove move = m_randomGraphMCMC.proposeEdgeMove();
-
-        move.display();
 
         double logLikelihoodRatio = m_dynamics.getLogLikelihoodRatioFromGraphMove(move);
         if (m_betaLikelihood == 0)
@@ -28,24 +24,13 @@ bool DynamicsMCMC::doMetropolisHastingsStep() {
         else
             logLikelihoodRatio *= m_betaLikelihood;
 
-        std::cout << "logLikelihoodRatio = " << logLikelihoodRatio << std::endl;
-
         double logPriorRatio = m_dynamics.getLogPriorRatioFromGraphMove(move);
         if (m_betaPrior == 0)
             logPriorRatio = 0;
         else
             logPriorRatio *= m_betaPrior;
 
-        std::cout << "logPriorRatio = " << logPriorRatio << std::endl;
-
-
-
         double LogProposalProbRatio = m_randomGraphMCMC.getLogProposalProbRatioFromGraphMove(move);
-
-
-        std::cout << "LogProposalProbRatio = " << LogProposalProbRatio << std::endl;
-
-
 
         m_lastLogJointRatio = logLikelihoodRatio + logPriorRatio;
 
@@ -56,18 +41,12 @@ bool DynamicsMCMC::doMetropolisHastingsStep() {
         m_isLastAccepted = false;
 
 
-        std::cout << "Trying out move" << std::endl;
-
         if (m_uniform(rng) < exp(m_lastLogAcceptance)){
-
-            std::cout << "move accepted" << std::endl;
 
             m_isLastAccepted = true;
             m_dynamics.applyGraphMove(move);
             m_randomGraphMCMC.updateProbabilitiesFromGraphMove(move);
         }
-
-        std::cout << "finish" << std::endl;
 
         return m_isLastAccepted;
     }
