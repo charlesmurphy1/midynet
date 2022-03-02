fbd3872c260a31db8eea2ba2ace1f37c78d105da
Charles Murphy <charles.murphy.1@ulaval.ca>
2022-03-02 13:34:54 -0500

    remove print in rng

--- _midynet/src/rng.cpp
+++ _midynet/src/rng.cpp
@@ -10,7 +10,7 @@ namespace FastMIDyNet {
 
 RNG rng;
 
-void seed(size_t seed){ rng.seed(seed); std::srand(seed); std::cout << "new seed: " << seed << std::endl; }
+void seed(size_t seed){ rng.seed(seed); std::srand(seed); }
 void seedWithTime(){
     seed(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
 }
