c5c20c9234f9f962c2e45ca8f602eb9d19ce956e
charlesmurphy1 <charles.murphy.1@ulaval.ca>
2022-03-21 13:33:09 -0400

    new script for fig4

--- scripts/midynet/figure4-nbinom.py
+++ scripts/midynet/figure4-nbinom.py
@@ -13,7 +13,12 @@ from load_specs import (
 
 
 def get_config(
-    dynamics="sis", num_procs=4, time="24:00:00", mem=12, seed=None
+    dynamics="sis",
+    num_procs=4,
+    num_async_process=1,
+    time="24:00:00",
+    mem=12,
+    seed=None,
 ):
     config = ExperimentConfig.default(
         f"figure4-nbinom-{dynamics}",
@@ -23,27 +28,24 @@ def get_config(
         path=PATH_TO_DATA / "figure4",
         seed=seed,
         num_procs=num_procs,
+        num_async_process=num_async_process,
     )
     N, E = 100, 250
-    T = [1000]
+    T = [100]
     h = [0, 0.5, 1]
     if dynamics == "sis":
         coupling = np.unique(
-            np.concatenate([np.linspace(0, 0.75, 10), np.linspace(0.75, 1, 10)])
+            np.concatenate(
+                [np.linspace(0, 1., 20), np.linspace(1., 2, 20)]
+            )
         )
     elif dynamics == "ising":
         coupling = np.unique(
-            np.concatenate([np.linspace(0, 1, 10), np.linspace(1, 2, 10)])
+            np.concatenate([np.linspace(0, 1, 20), np.linspace(1, 4, 20)])
         )
     elif dynamics == "cowan":
         coupling = np.unique(
-            np.concatenate(
-                [
-                    np.linspace(0, 1, 5),
-                    np.linspace(1, 2, 10),
-                    np.linspace(2, 4, 5),
-                ]
-            )
+            np.concatenate([np.linspace(1, 2, 20), np.linspace(2, 6, 20)])
         )
     config.graph.set_value("size", N)
     config.graph.edge_count.set_value("state", E)
@@ -55,9 +57,10 @@ def get_config(
     if dynamics == "sis" or dynamics == "cowan":
         config.dynamics.set_value("auto_activation_prob", 0.001)
     config.metrics.mutualinfo.set_value("num_samples", num_procs)
-    config.metrics.mutualinfo.set_value("method", "meanfield")
+    config.metrics.mutualinfo.set_value("method", "full-meanfield")
+    config.metrics.mutualinfo.set_value("initial_burn", 2000)
     config.metrics.mutualinfo.set_value("num_sweeps", 500)
-    config.metrics.mutualinfo.set_value("burn_per_vertex", 3)
+    config.metrics.mutualinfo.set_value("burn_per_vertex", 5)
 
     resources = {
         "account": "def-aallard",
@@ -72,8 +75,10 @@ def get_config(
 
 
 def main():
-    for dynamics in ["ising", "sis", "cowan"]:
-        config = get_config(dynamics, num_procs=40, mem=12, time="24:00:00")
+    for dynamics in ["cowan"]:
+        config = get_config(
+            dynamics, num_procs=40, mem=12, time="4:00:00"
+        )
         script = ScriptManager(
             executable=PATH_TO_RUN_EXEC["run"],
             execution_command=EXECUTION_COMMAND,
