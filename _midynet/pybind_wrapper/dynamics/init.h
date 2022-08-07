48c833a166a85a8905b5a96c3dc19734a64055b0
charlesmurphy1 <charles.murphy.1@ulaval.ca>
2022-08-07 10:38:50 -0400

    wrapping dynamics

--- _midynet/pybind_wrapper/dynamics/init.h
+++ _midynet/pybind_wrapper/dynamics/init.h
@@ -12,21 +12,27 @@ namespace FastMIDyNet{
 void initDynamics(py::module& m){
     declareDynamicsBaseClass<RandomGraph>(m, "Dynamics");
     declareDynamicsBaseClass<BlockLabeledRandomGraph>(m, "BlockLabeledDynamics");
+    declareDynamicsBaseClass<NestedBlockLabeledRandomGraph>(m, "NestedBlockLabeledDynamics");
 
     declareBinaryDynamicsBaseClass<RandomGraph>(m, "BinaryDynamics");
     declareBinaryDynamicsBaseClass<BlockLabeledRandomGraph>(m, "BlockLabeledBinaryDynamics");
+    declareBinaryDynamicsBaseClass<NestedBlockLabeledRandomGraph>(m, "NestedBlockLabeledBinaryDynamics");
 
     declareCowanDynamicsBaseClass<RandomGraph>(m, "CowanDynamics");
     declareCowanDynamicsBaseClass<BlockLabeledRandomGraph>(m, "BlockLabeledCowanDynamics");
+    declareCowanDynamicsBaseClass<NestedBlockLabeledRandomGraph>(m, "NestedBlockLabeledCowanDynamics");
 
     declareDegreeDynamicsBaseClass<RandomGraph>(m, "DegreeDynamics");
     declareDegreeDynamicsBaseClass<BlockLabeledRandomGraph>(m, "BlockLabeledDegreeDynamics");
+    declareDegreeDynamicsBaseClass<NestedBlockLabeledRandomGraph>(m, "NestedBlockLabeledDegreeDynamics");
 
     declareGlauberDynamicsBaseClass<RandomGraph>(m, "GlauberDynamics");
     declareGlauberDynamicsBaseClass<BlockLabeledRandomGraph>(m, "BlockLabeledGlauberDynamics");
+    declareGlauberDynamicsBaseClass<NestedBlockLabeledRandomGraph>(m, "NestedBlockLabeledGlauberDynamics");
 
     declareSISDynamicsBaseClass<RandomGraph>(m, "SISDynamics");
     declareSISDynamicsBaseClass<BlockLabeledRandomGraph>(m, "BlockLabeledSISDynamics");
+    declareSISDynamicsBaseClass<NestedBlockLabeledRandomGraph>(m, "NestedBlockLabeledSISDynamics");
 
 
 
