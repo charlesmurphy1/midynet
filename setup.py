124b51a78315698364832067f7cd870b6dac821b
charlesmurphy1 <charles.murphy.1@ulaval.ca>
2022-08-30 13:14:33 -0400

    optional install of graphinf

--- setup.py
+++ setup.py
@@ -7,13 +7,8 @@ from setuptools import setup, find_packages
 
 
 description = "Package for the analysis of stochastic processes on random graphs with information theory."
-# if importlib.util.find_spec("graphinf") is None:
-#     current_dir = os.getcwd()
-#     p = pathlib.Path("modules/graphinf/")
-#     os.chdir(p)
-#     os.chdir(current_dir)
-# print("Installing graphinf")
-os.system("pip install modules/graphinf")
+if importlib.util.find_spec("graphinf") is None:
+    os.system("pip install modules/graphinf")
 
 
 setup(
