276ac12813e30e05aa362ec477c28ef26d4bcbfe
Charles Murphy <charles.murphy.1@ulaval.ca>
2022-09-17 18:05:26 -0400

    modif explo script

--- scripts/recon/exploration/glauber.py
+++ scripts/recon/exploration/glauber.py
@@ -15,21 +15,28 @@ def get_config(
     expname=None,
     graph_prior="erdosrenyi",
     data_model="glauber",
+    size=100,
+    edge_count=250,
+    length=1000,
+    method="meanfield",
     num_procs=32,
     time="24:00:00",
     mem=12,
     seed=None,
 ):
-    path_to_exp = (
-        PATH_TO_DATA / "small-exploration" / f"recon-{graph_prior}-{data_model}-mf"
-    )
-    data_model = "glauber"
     likelihood_type = "uniform"
     if graph_prior == "degree_corrected_stochastic_block_model":
         graph_prior = "stochastic_block_model"
         likelihood_type = "degree_corrected"
     block_prior_type = "hyper"
     degree_prior_type = "hyper"
+
+    samples_per_proc = 1 if size > 6 else 100
+
+    expname = f"recon-{graph_prior}-{data_model}-{method}"
+    if size < 6:
+        expname = "small-" + expname
+    path_to_exp = PATH_TO_DATA / "exploration" / expname
 
     if expname is None:
         expname = f"recon-{graph_prior}-{data_model}"
@@ -42,25 +49,24 @@ def get_config(
         num_procs=num_procs,
         seed=seed,
     )
-    N = 4
-    E = 4
-    T = 100
     coupling = np.concatenate([np.linspace(0, 2, 20)])
     config.data_model.set_value("coupling", coupling)
-    config.data_model.set_value("num_active", int(N / 2))
-    config.graph_prior.set_value("size", N)
+    config.data_model.set_value("num_active", int(size / 2))
+    config.graph_prior.set_value("size", size)
     config.graph_prior.set_value("likelihood_type", likelihood_type)
     config.graph_prior.set_value("block_prior_type", block_prior_type)
     config.graph_prior.set_value("degree_prior_type", degree_prior_type)
-    config.graph_prior.set_value("edge_count", E)
-    config.data_model.set_value("num_steps", T)
-    config.metrics.recon_information.set_value("num_samples", 10 * num_procs)
+    config.graph_prior.set_value("edge_count", edge_count)
+    config.data_model.set_value("num_steps", length)
+    config.metrics.recon_information.set_value(
+        "num_samples", samples_per_proc * num_procs
+    )
     config.metrics.recon_information.set_value("burn_per_vertex", 10)
     config.metrics.recon_information.set_value("start_from_original", False)
     config.metrics.recon_information.set_value("equilibrate_mode_cluster", True)
     config.metrics.recon_information.set_value("initial_burn", 2000)
     config.metrics.recon_information.set_value("num_sweeps", 500)
-    config.metrics.recon_information.set_value("method", "meanfield")
+    config.metrics.recon_information.set_value("method", method)
 
     resources = {
         "account": "def-aallard",
@@ -80,9 +86,16 @@ def main():
         "stochastic_block_model",
         "degree_corrected_stochastic_block_model",
     ]:
-        expname = f"recon-{graph_prior}-glauber"
         config = get_config(
-            expname, graph_prior=graph_prior, num_procs=1, time="24:00:00", mem=12
+            data_model="glauber",
+            graph_prior=graph_prior,
+            size=4,
+            edge_count=4,
+            length=100,
+            method="exact",
+            num_procs=1,
+            time="12:00:00",
+            mem=12,
         )
         script = ScriptManager(
             executable=PATH_TO_RUN_EXEC["run"],
